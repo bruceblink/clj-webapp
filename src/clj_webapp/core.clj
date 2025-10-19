ed6451fd7273326111e12047095298b2ad67b295
likanug <likanug.g@qq.com>
2025-10-19 13:06:14 +0800

    feat: 新增"/snippet" API及单元测试

--- src/clj_webapp/core.clj
+++ src/clj_webapp/core.clj
@@ -1,13 +1,35 @@
 (ns clj-webapp.core
-  (:require [compojure.core :refer :all]
+  (:require [clojure.edn :as edn]
+            [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+;; 定义一个 snippets的promise
+(def snippets (repeatedly promise))
+
+;; 更新 snippet
+(defn accept-snippet [n text]
+  (deliver (nth snippets n) text)
+  )
+
+(future
+  (doseq [snippet (map deref snippets)]
+    (println snippet)
+    )
+  )
+
 ;; 定义路由
 (defroutes app-routes
            (GET "/" [] "Hello World")
            (GET "/hello/:name" [name]
              (str "<h1>Hello, " name "!</h1>"))
+           ;; 定义分片路由
+           (PUT "/snippet/:n" [n :as {:keys [body]}]
+             (accept-snippet (edn/read-string n) (slurp body))
+             (str "OK")
+             )
+
            (POST "/api/echo" {body :body}
              (str "You posted: " (slurp body)))
            (route/not-found "Not Found"))
--- test/clj_webapp/core_test.clj
+++ test/clj_webapp/core_test.clj
@@ -11,4 +11,15 @@
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
-      (is (= (:status response) 404)))))
+      (is (= (:status response) 404))))
+
+  (testing "snippet route"
+    (let [response (app (mock/request :put "/snippet/0" "Twas brilling, and the slithy toves"))]
+      (is (= (:status response) 200))
+      (is (= (:body response) "OK"))
+      )
+    )
+
+  )
+
+
