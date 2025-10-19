d052ffb4545e845a58e96661a285b00fa2e7eca4
likanug <likanug.g@qq.com>
2025-10-19 12:14:25 +0800

    chore: 更新项目依赖

--- project.clj
+++ project.clj
@@ -2,11 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [compojure "1.6.1"]
-                 [ring/ring-defaults "0.3.2"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [compojure "1.7.0"]
+                 [ring/ring-defaults "0.4.0"]
+                 [ring/ring-jetty-adapter "1.10.0"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler cli-webapp.handler/app}
+  :ring {:handler cli-webapp.core/app}
+  :main ^:skip-aot cli-webapp.core
+  :target-path "target/%s"
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                         [ring/ring-mock "0.3.2"]]}})
