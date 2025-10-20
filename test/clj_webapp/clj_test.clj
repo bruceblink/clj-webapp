40195dfa66cc6d67843bad9df876612388fe4f10
likanug <likanug.g@qq.com>
2025-10-20 11:51:56 +0800

    test: 更新单元测试语法格式

--- test/clj_webapp/clj_test.clj
+++ test/clj_webapp/clj_test.clj
@@ -5,17 +5,12 @@
 
 (deftest atom-def-test
   (testing "deref atom"
-    (is 42 @my-atom)
-    (is 42 (deref my-atom))
-    (is @my-atom (deref my-atom))
-    )
-  )
+    (is (= 42 @my-atom))
+    (is (= 42 (deref my-atom)))
+    (is (= @my-atom (deref my-atom)))))
 
 (deftest atom-update
   (testing "update atom1"
-    (is 43 (swap! my-atom inc))
-    )
+    (is (= 43 (swap! my-atom inc))))
   (testing "update atom2"
-    (is 44 (swap! my-atom + 2))
-    )
-  )
+    (is (= 45 (swap! my-atom + 2)))))  ;; 注意这里是从 43 -> 45
