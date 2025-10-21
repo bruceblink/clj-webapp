f999a142aa1a817fd0c843b489fd3511f49dacfa
likanug <likanug.g@qq.com>
2025-10-21 12:51:16 +0800

    feat: 新增"/session"相关的API

--- src/clj_webapp/routes.clj
+++ src/clj_webapp/routes.clj
@@ -1,16 +1,18 @@
 (ns clj-webapp.routes
   (:require [cheshire.core :as json]
+            [clj-webapp.session :refer [create-session get-session]]
+            [clj-webapp.translate :refer [get-translation]]
             [clojure.edn :as edn]
             [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.util.response :refer [response status]]
-            [cheshire.core :as json] ))
+            ))
 
 ;; 定义 snippets
 (def snippets (repeatedly promise))
 
-(defn accept-snippet [n text]
-  (deliver (nth snippets n) text)
+(defn accept-snippet [session n text]        ;; 新增支持session
+  (deliver (nth (:snippets session) n) text)
   )
 
 ;; 后台异步打印提交的 snippet
@@ -38,14 +40,28 @@
            (GET "/hello/:name" [name]
              (str "<h1>Hello, " name "!</h1>"))
 
-           (PUT "/snippet/:n" [n :as {:keys [body]}]
-             (accept-snippet (edn/read-string n) (slurp body))
-             "OK")
-
            (POST "/api/echo" {body :body}
              (str "You posted: " (slurp body)))
 
            (GET "/players" [] (list-players))
            (PUT "/players/:player-name" [player-name] (creat-player player-name))
+
+           (POST "/session/create" []
+             (response (str (create-session)))
+             )
+           (context "/session/:session-id" [session-id]
+             (let [session (get-session (edn/read-string session-id))]
+               (routes
+                 (PUT "/snippet/:n" [n :as {:keys [body]}]
+                   (accept-snippet session (edn/read-string n) (slurp body))
+                   (response "OK")
+                   )
+
+                 (GET "/translation/:n" [n]
+                   (response (get-translation session (edn/read-string n)))
+                   )
+                 )
+               )
+             )
 
            (route/not-found "Not Found"))
