058c2399abaf3892cea153195029b403fe2009d2
likanug <likanug.g@qq.com>
2025-10-21 14:00:27 +0800

    fix: 修复请求头无法转换成json的问题

--- src/clj_webapp/middleware.clj
+++ src/clj_webapp/middleware.clj
@@ -14,11 +14,12 @@
 
 (defn wrap-base [handler]
   (-> handler
-      wrap-charset ;; 自定义字符编码中间件
-      (wrap-defaults api-defaults) ;; 默认配置中间件
-      wrap-params  ;; 解析 URL 参数
+      ;; 请求头需要添加 "Content-Type": "application/json" 参数
+      (wrap-json-body {:keywords? true}) ;; 自动解析 JSON 请求体 ✅ 关键点：开启关键字转换
       wrap-keyword-params ;; 将参数名转换为关键字
-      wrap-json-body ;; 自动解析 JSON 请求体
+      wrap-params  ;; 解析 URL 参数
+      (wrap-defaults api-defaults) ;; 默认配置中间件
       wrap-json-response  ;; 自动返回 JSON 响应
+      wrap-charset ;; 自定义字符编码中间件
       )
   )
