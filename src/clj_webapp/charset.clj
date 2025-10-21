dcc39b49843cd426f17bce65d9db4054b3d21c65
likanug <likanug.g@qq.com>
2025-10-21 12:24:10 +0800

    refactor: 重构中间件middleware模块，整合charset.clj到middleware中，组合多个中间件

--- src/clj_webapp/charset.clj
+++
@@ -1,8 +0,0 @@
-(ns clj-webapp.charset
-  (:require [ring.util.response :refer [charset]]))
-
-(defn wrap-charset
-  "转换字符"
-  [handler]
-  (fn [req] (charset (handler req) "UTF-8"))
-  )
--- src/clj_webapp/middleware.clj
+++ src/clj_webapp/middleware.clj
@@ -1,9 +1,18 @@
 (ns clj-webapp.middleware
-  (:require [ring.middleware.defaults :refer [api-defaults wrap-defaults]])
+  (:require [ring.middleware.defaults :refer [api-defaults wrap-defaults]]
+            [ring.util.response :refer [charset]] ))
+
+(defn wrap-charset
+  "转换字符编码的中间件"
+  [handler]
+  (fn [req] (charset (handler req) "UTF-8"))
   )
 
 ;; 一个帮助函数，可以根据环境选择不同默认中间件
 (defn wrap-base [handler]
   ;; 生产环境可以换成 site-defaults
-  (wrap-defaults handler api-defaults)
+  (-> handler
+      wrap-charset
+      (wrap-defaults api-defaults)
+    )
   )
