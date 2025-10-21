49e25eb97dad51358daae807126e3d19d8301223
likanug <likanug.g@qq.com>
2025-10-21 23:46:56 +0800

    chore: 更新clojure和schejulure的依赖版本

--- project.clj
+++ project.clj
@@ -2,14 +2,14 @@
   :description "一个使用compojure开发的 Clojure web app "
   :url "https://github.com/bruceblink/clj-webapp"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.12.3"]
                  [compojure "1.7.0"]
                  [ring/ring-defaults "0.4.0"]
                  [ring/ring-jetty-adapter "1.10.0"]
                  [ring/ring-json "0.5.1"]
                  [cheshire "6.1.0"]
                  [clj-http "3.13.1"]
-                 [schejulure "0.1.4"]
+                 [schejulure "1.0.1"]
                  [org.flatland/useful "0.10.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler clj-webapp.core/app}
